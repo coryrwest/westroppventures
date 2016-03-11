ec786b1384335ef603fa3cc8d5cdcdbe20d41e5a
Cory Westropp <cory.r.west@gmail.com>
2016-03-10 16:09:24 -0800

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,15 @@
+var app = angular.module('app', ['ngResource']);
+
+app.controller('HomeCtrl', ['$scope', function ($scope) {
+//    dataService.getList({object: 'pick', limit: numberDays * 7}, function(obj) {
+//    });
+
+
+}]);
+
+
+//app.factory('dataService', ['$q', '$resource', function ($q, $resource) {
+//    return $resource('data/:object/:limit', { object: '@object', limit: '@limit' }, {
+//        getList: { method: 'GET', isArray: true }
+//    });
+//}]);
